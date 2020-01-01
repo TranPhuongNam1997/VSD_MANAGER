5b15e303d5d74bac50793906f5900a0776551570
namtp <namtp@bave.io>
2020-01-01 21:02:50 +0700

    update

--- libs/js/script.js
+++ libs/js/script.js
@@ -45,8 +45,28 @@ $( document ).ready(function() {
        $(this).toggleClass('active');
     });
 
+    $('.btn-selectall input').click(function () {
+        $('.btn-selectchild input').prop('checked', this.checked);
+    });
+    $('.btn-selectchild input').change(function () {
+        var check = ($('.btn-selectchild input').filter(":checked").length == $('.btn-selectchild input').length);
+        $('.btn-selectall input').prop("checked", check);
+    });
 
-    // $('.btn-selectall input').click(function () {
+    // $('.btn-selectall').click(function () {
+    //     $('.btn-selectchild').prop('checked', this.checked);
+    //     $('.check-1 label').toggleClass('active');
+    //
+    // });
+    // $('.btn-selectchild').change(function () {
+    //     var check = ($('.btn-selectchild').filter(":checked").length == $('.btn-selectchild').length);
+    //     $(this).next().toggleClass('active');
+    //     $('.btn-selectall').prop("checked", check);
+    // });
+
+    // $('.btn-selectall').click(function () {
+    //     $('.btn-general input').toggle('checked','checked');
+    //     $('.btn-general').toggleClass('active');
     //     $('.btn-selectchild input').prop('checked', this.checked);
     // });
     // $('.btn-selectchild input').change(function () {
@@ -54,15 +74,57 @@ $( document ).ready(function() {
     //     $('.btn-selectall input').prop("checked", check);
     // });
 
-    $('.btn-selectall').click(function () {
-        $('.btn-general input').toggle('checked','checked');
-        $('.btn-general').toggleClass('active');
-        $('.btn-selectchild input').prop('checked', this.checked);
-    });
-    $('.btn-selectchild input').change(function () {
-        var check = ($('.btn-selectchild input').filter(":checked").length == $('.btn-selectchild input').length);
-        $('.btn-selectall input').prop("checked", check);
-    });
+
+        // $('#customCheck').on('click',function(){
+        //     if(this.checked){
+        //         $('.btn-selectchild').each(function(){
+        //             this.checked = true;
+        //             // $(this).next().addClass('active');
+        //             $(this).next().attr('style','background: red');
+        //             // $('#customCheck').next().addClass('active');
+        //
+        //         });
+        //     }else{
+        //         $('.btn-selectchild').each(function(){
+        //             this.checked = false;
+        //             // $(this).next().removeClass('active');
+        //             // $('#customCheck').next().removeClass('active');
+        //
+        //             $(this).next().attr('style','background: transparent');
+        //         });
+        //     }
+        // });
+        //
+        // $('.btn-selectchild').on('click',function(){
+        //     if($('.btn-selectchild:checked').length == $('.btn-selectchild').length){
+        //         $('#customCheck').prop('checked',true);
+        //         // $(this).next().addClass('active');
+        //         $(this).next().attr('style','background: red');
+        //     }else{
+        //         $('#customCheck').prop('checked',false);
+        //         // $(this).next().removeClass('active');
+        //         $(this).next().attr('style','background: transparent');
+        //     }
+        // });
+
+    // var $tbl = $('.check-1');
+    // var $bodychk = $tbl.find('input');
+    //
+    // $bodychk.on('change', function () {
+    //     if($(this).is(':checked')) {
+    //         $(this).next().addClass('active');
+    //     }
+    //     else {
+    //         $(this).next().removeClass('active');
+    //     }
+    // });
+    //
+    // $tbl.find('input.btn-selectall:checkbox').change(function () {
+    //     var c = this.checked;
+    //     $bodychk.prop('checked', c);
+    //     $bodychk.trigger('change');
+    // });
+
 
     // custom datepicker
 
@@ -116,57 +178,6 @@ $( document ).ready(function() {
 
                 }
         });
-
-});
-$(function() {
-
-    // slider js
-    $(".slider-news").owlCarousel({
-        items: 1,
-        responsive: {
-            1200: { item: 3, },// breakpoint from 1200 up
-            982: { items: 3, },
-            768: { items: 2, },
-            480: { items: 1, },
-            0: { items: 1, }
-        },
-        rewind: false,
-        autoplay: true,
-        autoplayTimeout: 5000,
-        autoplayHoverPause: true,
-        smartSpeed: 500, //slide speed smooth
-
-        dots: true,
-        dotsEach: false,
-        loop: true,
-        nav: true,
-        navText: ['', ''],
-        // navSpeed: 250, //slide speed when click button
-
-        autoWidth: false,
-        margin: 11,
-
-        lazyContent: false,
-        lazyLoad: false,
-
-        animateIn: false,
-        animateOut: false,
-
-        center: false,
-        URLhashListener: false,
-
-        video: false,
-        videoHeight: false,
-        videoWidth: false,
-    });
-
-    //datetimepicker
-    // $('input[name="daterange"]').daterangepicker({
-    //     opens: 'left'
-    // }, function(start, end, label) {
-    //     console.log("A new date selection was made: " + start.format('YYYY-MM-DD') + ' to ' + end.format('YYYY-MM-DD'));
-    // });
-
 
 });
 

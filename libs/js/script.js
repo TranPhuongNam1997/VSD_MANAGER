7e8d0e8d66e6375df633d2e2ce2747dcc2bd49de
TranPhuongNam <tranphuongnam17041997@gmail.com>
2020-02-04 17:07:40 +0700

    update

--- libs/js/script.js
+++ libs/js/script.js
@@ -245,7 +245,8 @@ $( document ).ready(function() {
     $('input[name="daterange"]').daterangepicker({
         opens: 'left',
         "autoApply": true
-    }, function(start, end, label) {
+    },
+        function(start, end, label) {
         console.log("A new date selection was made: " + start.format('YYYY-MM-DD') + ' to ' + end.format('YYYY-MM-DD'));
     });
     //checkvalidate
